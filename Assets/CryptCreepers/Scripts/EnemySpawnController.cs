515c0598e4de6745787ce7263a34582e2377985a
Juan Sanchez <gzki86@gmail.com>
2023-01-15 16:55:58 -0600

    Adding spawn rate variable and fixing the enemy type spawn, class 19 of the course

--- Assets/CryptCreepers/Scripts/EnemySpawnController.cs
+++ Assets/CryptCreepers/Scripts/EnemySpawnController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemySpawnController : MonoBehaviour
 {
+    [Range(1, 10)][SerializeField]float spawnRate = 1;
     [SerializeField] GameObject[] enemyPrefab;
     
      // Start is called before the first frame update
@@ -15,7 +16,7 @@ public class EnemySpawnController : MonoBehaviour
     IEnumerator SpanwnNewEnemy(){
         while (true)
         {
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(1/spawnRate);
             float random = Random.Range(0.0f, 1.0f);
 
             if(random < GameManager.Instance.difficulty * 0.1f){
